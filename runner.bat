48d1cc374e088011ab0e0da1465c88df8e9a7a9b
Nelson Orellana <55712980+Nels2@users.noreply.github.com>
2021-11-12 09:58:51 -0600

    create .bat & .ps1 files to run on windows w ease

---
+++ runner.bat
@@ -0,0 +1,1 @@
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './test.ps1'"
---
+++ start.ps1
@@ -0,0 +1,4 @@
+Function pwrg {python C:\Users\Norellana\Downloads\randompass-main\Rpass.py}
+Set-Alias -Name pwg -Value pwrg
+pwg
+Start-Sleep -Seconds 120
