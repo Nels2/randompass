b8d8016106d3f30d0cd258dd020660988a657c59
Nelson Orellana <55712980+Nels2@users.noreply.github.com>
2021-11-12 10:00:30 -0600

    update to work on any machine
    update to so it works without needed edits

--- start.ps1
+++ start.ps1
@@ -1,4 +1,5 @@
-Function pwrg {python C:\Users\Norellana\Downloads\randompass-main\Rpass.py}
+$usr= ((Get-WMIObject -ClassName Win32_ComputerSystem).Username).Split('\')[1]
+Function pwrg {python C:\Users\$usr\Downloads\randompass-main\Rpass.py}
 Set-Alias -Name pwg -Value pwrg
 pwg
 Start-Sleep -Seconds 120
