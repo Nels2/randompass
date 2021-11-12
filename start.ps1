46ff76c146d690e4fd2601c203c0ddb52d394275
Nelson Orellana <55712980+Nels2@users.noreply.github.com>
2021-11-12 10:21:44 -0600

    Delete start.ps1

--- start.ps1
+++
@@ -1,5 +0,0 @@
-$usr= ((Get-WMIObject -ClassName Win32_ComputerSystem).Username).Split('\')[1]
-Function pwrg {python C:\Users\$usr\Downloads\randompass-main\Rpass.py}
-Set-Alias -Name pwg -Value pwrg
-pwg
-Start-Sleep -Seconds 120
