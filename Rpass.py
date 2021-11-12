742edb52438d6cc894b2e4c5a80a0b43fb58bf75
Nelson Orellana <55712980+Nels2@users.noreply.github.com>
2021-11-12 16:50:26 -0600

    add ability to store passwords in a txt file

--- Rpass.py
+++ Rpass.py
@@ -1,16 +1,21 @@
-# rnadom password generator.
-
+# .Sypnosis: this is a random password generator. saves to a text file after the passwords are generated.
 import random
 
 char = 'abcdefghijklmnopqrstuv1234567890ABCDEFGHIJKLMNOPQRSTUV!@#$%^&*'
-number = input('Number of PWs: ')
+number = input('Number of passwords to create: ')
 number = int(number)
-
 length = input('Password length: ')
 length = int(length)
 
+f = open('createdPasswords.txt', "wb")
+f.close()
 for p in range(number):
     password = ''
     for c in range(length): 
         password += random.choice(char)
+    x = password
+    f = open('createdPasswords.txt', 'a')
+    f.write(x + "\n")
+    f.close()
     print(password)
+print("you can view/copy the ("+ str(number) +") created passwords in createdPasswords.txt")
